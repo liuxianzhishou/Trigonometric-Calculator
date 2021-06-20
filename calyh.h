84612cb877cfd9fc5aa8984ac47a12b1956b9fe9
liuxianzhishou <63947968+liuxianzhishou@users.noreply.github.com>
2021-06-20 15:09:34 +0800

    this is a scene of the calculator
    developed by c++ with QT

---
+++ calyh.h
@@ -0,0 +1,26 @@
+#ifndef CALYH_H
+#define CALYH_H
+
+#include <QDialog>
+
+namespace Ui {
+class CalYh;
+}
+
+class CalYh : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CalYh(QWidget *parent = 0);
+    ~CalYh();
+
+private:
+    Ui::CalYh *ui;
+    void layoutYh();
+    QString s;//控件显示的字符串
+    bool isClear;//判断已经按过sin/cos/arcsin/arctan按钮，按过表示true，默认false
+
+};
+
+#endif // CALYH_H
